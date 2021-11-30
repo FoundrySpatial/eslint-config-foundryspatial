35ed1924d2db1323dd0c59376c35be3fb32bf474
Duncan MacKenzie <duncan@foundryspatial.com>
2021-11-30 11:37:25 -0800

    add our latest eslint preferences as of Nov 2021
    version bump package, dependencies

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Foundry Spatial code style rules to override AirBnB base
 
 module.exports = {
-
     extends: [
         'airbnb-base',
+    ],
+
+    plugins: [
+        'simple-import-sort',
+        'jsdoc',
     ],
 
     rules: {
@@ -12,6 +16,7 @@ module.exports = {
             'error',
             4,
         ],
+
         // max line length 120 chars
         'max-len': ['error', 120, 2, {
             ignoreUrls: true,
@@ -20,8 +25,13 @@ module.exports = {
             ignoreStrings: true,
             ignoreTemplateLiterals: true,
         }],
+
         // Disable no-underscore-dangle (mapbox-gl, elasticsearch)
         'no-underscore-dangle': 'off',
+        'no-undefined': 'error',
+        'simple-import-sort/sort': 'error',
+        'sort-imports': 'off', // must turn off for simple-import-sort/sort to work
+        'import/order': 'off', // must turn off for simple-import-sort/sort to work
+        'jsdoc/no-undefined-types': 'off',
     },
-
 };
